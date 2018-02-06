2c6b67124553963aa08af04fde1291f2379d88e5
yokochi47 <yokochi47@gmail.com>
2018-02-06 18:13:27 +0900

    Support git cloned version

--- scripts/xtool-home.sh
+++ scripts/xtool-home.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [ -e VERSION ] ; then
+if [ -d ../BMRBxTool ] || [ -d ../../BMRBxTool] ; then
+ BMRBX_TOOL_HOME=../BMRBxTool
+elif [ -e VERSION ] ; then
  BMRBX_TOOL_HOME=../bmrbx-tool-`cat VERSION` # Please edit me.
 elif [ -e ../VERSION ] ; then
  BMRBX_TOOL_HOME=../bmrbx-tool-`cat ../VERSION` # Please edit me.
