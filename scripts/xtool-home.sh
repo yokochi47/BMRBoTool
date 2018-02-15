2d7f334bff41ede1b0bf43e5d5d46c56c0e120f9
Masashi Yokochi <yokochi47@gmail.com>
2018-02-15 13:10:08 +0900

    Add ontology relationship between BMRB/OWL and PDBx ontology

--- scripts/xtool-home.sh
+++ scripts/xtool-home.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ -d ../BMRBxTool ] || [ -d ../../BMRBxTool] ; then
+if [ -d ../BMRBxTool ] || [ -d ../../BMRBxTool ] ; then
  BMRBX_TOOL_HOME=../BMRBxTool
 elif [ -e VERSION ] ; then
  BMRBX_TOOL_HOME=../bmrbx-tool-`cat VERSION` # Please edit me.
