47fb381d2c2a80fe3f2ad335685fc51eb85f1c33
Masashi Yokochi <yokochi47@gmail.com>
2018-04-16 11:53:00 +0900

    Release v1.28.0

--- xsd2owl/bmrbx2owl.sh
+++ xsd2owl/bmrbx2owl.sh
@@ -2,6 +2,15 @@
 
 source ../scripts/saxon-home.sh
 
+NMRSTAR_DIC_FILE=../NMR-STAR.dic
+
+if [ -e $NMRSTAR_DIC_FILE ] ; then
+
+ arg=(`tr -d '\r' < $NMRSTAR_DIC_FILE | grep dictionary.version`)
+ DIC_VERSION=${arg[1]}
+
+fi
+
 TAGMAP2XML_XSL=tagmap2xml.xsl
 tagmap_csv_file=tagmap.csv
 tagmap_xml_file=tagmap.xml
@@ -10,7 +19,7 @@ java -jar ../$SAXON -it:main -xsl:$TAGMAP2XML_XSL -o:$tagmap_xml_file tagmap_csv
 
 SOURCE_XSD_FILE=../schema/mmcif_nmr-star.xsd
 
-java -jar ../$SAXON -s:$SOURCE_XSD_FILE -xsl:bmrbx2owl.xsl -o:../schema/mmcif_nmr-star.owl tagmap_xml_file=$tagmap_xml_file
+java -jar ../$SAXON -s:$SOURCE_XSD_FILE -xsl:bmrbx2owl.xsl -o:../schema/mmcif_nmr-star.owl dic_version=$DIC_VERSION tagmap_xml_file=$tagmap_xml_file
 
 #java -jar ../$SAXON -s:$SOURCE_XSD_FILE -xsl:../schema/xs3p.xsl -o:../schema/mmcif_nmr-star.xsd.html -versionmsg:off
 
